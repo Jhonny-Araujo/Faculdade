48ff0064db5d897a26fde9ae3f5913aafbf30c13
Quelton Jhones <49000606+Jhonny-Araujo@users.noreply.github.com>
2021-05-30 22:56:45 -0300

    Base de Dados 2

---
+++ PIC SEMESTRE 5/bd02.pl
@@ -0,0 +1,47 @@
+resposta("A ethernet constitui um conjunto de protocolos e técnicas para tornar viável a comunicação entre os computadores. É usada na grande maioria das redes de computadores LAN instaladas pelo mundo. Muitas vezes, o termo ethernet é empregado para definir as redes locais. Estudaremos as características dessa rede amplamente disseminada.","O que é Ethernet?").
+
+resposta("A ethernet oferece a comunicação entre equipamentos de uma mesma rede física sem o uso de conexões. Os seus serviços são  o Unicast, o Multicast e o Broadcast.","O que é Endereçamento na ethernet e quais são os seus serviços?").
+
+resposta("Media Access Control  no ortuguês: Controle de Acesso ao Meio, é um termo utilizado em redes de computadores para designar parte da camada de enlace, camada número 2 segundo o modelo OSI de 7 camadas (física, enlace, rede, transporte, sessão, apresentação e aplicação).
+","O que é um Protocolo MAC?").
+
+resposta("Criado em 1999, o padrão Gigabit (também conhecido como 10/100/1000) ainda é o mais recente dos tipos de rede. Ele promete velocidades de até 1 Gb/s – daí o nome –, 10 vezes maiores que o Fast Ethernet.
+","O que é Padrão Gigabit Ethernet?").
+
+resposta("É uma tecnologia que permite utilizar redes ethernet em áreas metropolitanas e geograficamente distribuídas. Esse conceito surgiu porque, de acordo com alguns estudos, o tráfego de dados estaria superando o tráfego de voz nas redes metropolitanas, portanto, seria mais interessante utilizar uma infraestrutura de transmissão de dados do que uma Time Division Multiplexing (TDM), criada para a transmissão de voz.
+","O que é Metro Ethernet?").
+
+resposta("É um conjunto de protocolos de comunicação. O nome vem de dois protocolos TCP (Transmission Control Protocol) e o IP (Internet Protocol). Ele tem por objetivo padronizar todas as comunicações de rede, principalmente as comunicações na web.
+","O que é o protocolo TCP/IP?").
+
+resposta("Existem diferentes protocolos que trabalham na camada de aplicação e os mais conhecidos  são o HTTP, SMTP, FTP, SNTP, DNS e Telnet.","Quais são os protocolos que constituem o TCP?").
+
+resposta("Este protocolo é proprietário da Cisco. Foi projetado com o objetivo de trazer melhorias para o protocolo RIP.","O que é IGRP?").
+
+resposta("Este protocolo foi desenvolvido pela Cisco. É considerado um protocolo híbrido combinando as melhores características dos protocolos para a definição de rotas com base em vetores de distância e em estado de ligação.
+","O que é EIGRP?").
+
+resposta("Simple Mail Transfer Protocol, utiliza os serviços da camada de transporte através do protocolo TCP para transferir mensagens. Apresenta uma estrutura organizada para a transmissão dos dados.
+","O que é SMTP?").
+
+resposta("É um protocolo para acesso remoto utilizado para configurações de máquinas. Fornece um serviço de terminal virtual. Uma vez estabelecida uma sessão de login remoto, Telnet disponibiliza mecanismos necessários para que os caracteres digitados na máquina local sejam passados diretamente à máquina remota.
+","O que é Telnet?").
+
+resposta("É um protocolo de transferência de arquivos entre dois sistemas que utiliza duas conexões: uma é empregada para dar suporte ao processo de transferência de dados e a outra conexão é utilizada para dar suporte aos vários processos de controle da sessão.
+","O que é FTP?").
+
+resposta("O protocolo de transferência e hipertexto é o protocolo em que a world wide web está baseada. É considerado um protocolo de pedido e resposta e funciona da seguinte forma: um programa cliente estabelece uma conexão TCP com um programa servidor HTTP. O programa servidor aceita essa conexão e responde ao pedido do cliente.
+","O que é http?").
+
+
+resposta("A camada de rede é quem fornece os serviços de transferência de dados fim a fim sobre uma rede, independentemente das características das subredes físicas. Está mais relacionada à topologia de rede e tem como uma de suas principais funções resolver problemas de roteamento em rede.
+","O que é camada de rede?").
+
+
+
+resposta("IPv4 significa Internet Protocol version 4, ou versão 4 de protocolos. É a tecnologia que permite que nossos aparelhos conectem na Internet, seja qual for o tipo de gadget – pode ser PC, Mac, smartphones ou outros aparelhos. Cada um que estiver online terá um código único, como 99.48.227.227 por exemplo, para enviar e receber dados de outros que estiverem conectados.
+","O que é IPv4?").
+
+
+resposta("O Internet Control Message Protocol é um protocolo da camada 3, usado para trocar mensagens entre equipamentos que estão interligados na rede. Em redes LAN, é empregado, pelos administradores de rede, para verificar se um determinado equipamento está devidamente conectado à rede.","O que é ICMP?").
+
